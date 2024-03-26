6e97cf91968f3b1a9c7ba8dd6d26fa09a2306edb
vinicius2129 <139911616+vinicius2129@users.noreply.github.com>
2024-03-26 00:20:06 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
-// Banco de Dados
-const mysql = require('mysql2');
+const mysql = require('mysql2'); // LEMBRAR: $ npm install mysql
 
-let connect = mysql.createConnection({
-  host: "",
-  user: "",
-  password: "",
-  database: ""
+// Configuração da conexão com o banco de dados
+const connection = mysql.createConnection({
+    host: process.env.host, // Endereço do servidor MySQL
+    port: process.env.port , // Porta do servidor MySQL
+    user: process.env.user, // Nome de usuário do MySQL
+    password: process.env.password, // Senha do usuário do MySQL
+    database: process.env.database // Nome do banco de dados que deseja se conectar
+  });
+
+// Conexão com o banco de dados
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão bem-sucedida ao banco de dados MySQL');
 });
-
-module.exports = connect;
+module.exports = connection;
