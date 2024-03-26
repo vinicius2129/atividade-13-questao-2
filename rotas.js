708bf72fe126e9d4d2462bdd1b52aa181b0182d5
vinicius2129 <139911616+vinicius2129@users.noreply.github.com>
2024-03-26 00:21:25 -0300

    Update rotas.js

--- rotas.js
+++ rotas.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => res.redirect('/login'));
 // Rota para abrir a página de login
 router.get('/login', (req, res) => {
   // Se estiver logado ir para a página inicial, Se não vai para tela de login
-  if (req.session?.login) {
+  if (req.session.login) {
     return res.redirect('/login/success');
   }
   // Verificar se há uma mensagem de erro na query string
@@ -22,7 +22,7 @@ router.post('/login', controle.logarUsuario);
 // Rota para a página de sucesso do login
 router.get('/login/success', (req, res) => {
   // Se estiver logado continua, Se não volta para tela de login
-  if (req.session?.login) {
+  if (req.session.login) {
     res.render('login-success');
   } else {
     res.redirect('/login');
@@ -32,7 +32,7 @@ router.get('/login/success', (req, res) => {
 // Rota para abrir a página de registro de login
 router.get('/register', (req, res) => {
   // Se estiver logado continua, Se não volta para tela de login
-  if (req.session?.login) {
+  if (req.session.login) {
     // Verificar se há uma mensagem de erro na query string
     const errorMessage = req.query.error ? 'Erro ao registrar usuário. Tente novamente.' : null;
     res.render('register', { error: errorMessage });
@@ -47,7 +47,7 @@ router.post('/register', controle.cadastrarUsuario);
 
 router.post('/sair-da-conta', (req, res) => {
    // Se estiver logado na conta, destruir sessão (deslogar)
-  if (req.session?.login) {
+  if (req.session.login) {
     return req.session.destroy(() => res.redirect('/login'));
   }
 });
