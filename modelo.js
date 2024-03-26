453a8fcbd433db3913a93b9429229ebd77e86b82
vinicius2129 <139911616+vinicius2129@users.noreply.github.com>
2024-03-26 00:20:29 -0300

    Update modelo.js

--- modelo.js
+++ modelo.js
@@ -1,19 +1,29 @@
-
-
 // Modelo de usuários | por enquanto sem banco de dados
-const users = [
-  { nome: 'exemplo', email: 'usuario1@example.com', password: 'senha1' },
-];
+const bd = require('./db.js')
 
 function adicionarUsuario(usuario) {
-  users.push(usuario);
+  const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
+  bd.query(query, [usuario.email, usuario.password], (err, result) => {
+      if (err) {
+          console.error('Erro ao registrar usuário:', err);
+          return;
+      }
+  });
 }
 
-function acharUsuario(email, senha) {
-  return users.find(user => user.email === email && user.password === senha);
+function acharUsuario(email, senha, callback) {
+  const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+  bd.query(query, [email, senha], (err, results) => {
+      if (err) {
+          console.error('Erro ao encontrar usuário:', err);
+          callback(err, null);
+          return;
+      }
+      callback(null, results[0]); // Retorna apenas o primeiro usuário encontrado
+  });
 }
 
 module.exports = {
   adicionarUsuario,
-  acharUsuario
+  acharUsuario
 };
